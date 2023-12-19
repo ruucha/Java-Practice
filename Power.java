821c879021e753538fa1b6bc70ccb36937b70042
ruucha <ruchapingale20@gmail.com>
2023-12-19 23:56:10 +0530

    power

--- Power.java
+++ Power.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 class Power {
     public static void main(String[] args) {
-        System.out.println("Hello Rucha!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int num = sc.nextInt();
+        System.out.println("Enter power :");
+        int pow = sc.nextInt();
+        double result = Math.pow(num, pow);
+        System.out.println(result);
     }
 }
